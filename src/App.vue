e37d67ed3e8b9ef02af36a8cba33287b5b13743b
Yury Glushko <yury.glushko.web@gmail.com>
2022-03-08 23:21:26 +0300

    added hw_3

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="formBtnHandler" class="formBtn">{{ formBtnCaption }}</button>
+    <TableForm v-show="isFormShow" @addData="addData" :tableLength="this.tableData.length"/>
+    <TableHeader :headerData="headerData"/>
+    <TableContent :tableData="this.getTableData"/>
+    <TablePaginator
+        :totalItems="tableData.length"
+        :pageSize="this.pageSize"
+        @setPage="setPage"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TableHeader from "@/components/TableHeader";
+import TableContent from "@/components/TableContent";
+import TableForm from "@/components/TableForm";
+import TablePaginator from "@/components/TablePaginator";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TablePaginator,
+    TableForm,
+    TableContent,
+    TableHeader
+  },
+  data() {
+    return {
+      headerData: ['#', "Date", 'Category', "Value"],
+      tableData: [
+        {
+          id: 1,
+          date: '08.03.2022',
+          category: 'Food',
+          value: '100'
+        },
+        {
+          id: 2,
+          date: '08.03.2022',
+          category: 'Transport',
+          value: '100'
+        },
+        {
+          id: 3,
+          date: '08.03.2022',
+          category: 'Clothing',
+          value: '100'
+        },
+        {
+          id: 4,
+          date: '08.03.2022',
+          category: 'Food',
+          value: '100'
+        },
+        {
+          id: 5,
+          date: '08.03.2022',
+          category: 'Transport',
+          value: '100'
+        },
+        {
+          id: 6,
+          date: '08.03.2022',
+          category: 'Clothing',
+          value: '100'
+        }
+      ],
+      isFormShow: false,
+      currentPage: 1,
+      pageSize: 5
+    }
+  },
+  computed: {
+    formBtnCaption() {
+      return this.isFormShow ? 'Close Form' : 'Open Form'
+    },
+    getTableData() {
+      const left = (this.currentPage - 1) * this.pageSize;
+      const right = this.currentPage * this.pageSize;
+      return this.tableData.filter((el, idx) => left <= idx && idx < right)
+    }
+  },
+  methods: {
+    formBtnHandler() {
+      this.isFormShow = !this.isFormShow;
+    },
+    addData(data) {
+      this.tableData.push(data);
+    },
+    setPage(page) {
+      this.currentPage = page
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 200px auto 0;
+  max-width: 1140px;
 }
 </style>
---
+++ src/components/TableHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="headerWrapper">
+    <div
+        v-for="caption in headerData"
+        :key="caption"
+        class="headerEl"
+    >
+      {{ caption }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TableHeader',
+  components: {},
+  props: {
+    headerData: {
+      type: Array,
+      default: () => []
+    },
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.headerWrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  justify-items: start;
+  margin-top: 10px;
+}
+
+.headerEl {
+  font-weight: bold;
+}
+</style>
